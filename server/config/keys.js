80f3dfe9cf8b2b4c702de31dfb08a573edebae50
Mario de Biase <debiasej@gmail.com>
2017-11-09 20:44:23 +0100

    Add passport and google strategy

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+// Set your OAuth Google tokens here
+module.exports = {
+  googleClientID: '66949193449-3qat6sri3355fnm4d82623a3h4mu5mj0.apps.googleusercontent.com',
+  googleClientSecret: 'dNHm9xoyI5IafcteXUEFE1DS'
+}
