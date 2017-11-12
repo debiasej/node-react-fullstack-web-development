8ba0fc44552cf6bf11aef5ec0917bc35d8b041ea
Mario de Biase <debiasej@gmail.com>
2017-11-12 20:16:05 +0100

    Refactor server

--- server/index.js
+++ server/index.js
@@ -1,34 +1,9 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys')
+require('./services/passport')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express()
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken)
-      console.log('refresh token', refreshToken)
-      console.log('profile', profile)
-    }
-  )
-)
-
-app.get('/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-)
-
-app.get('/auth/google/callback',
-  passport.authenticate('google')
-)
+authRoutes(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const passport = require('passport')
+
+const routes = (app) => {
+  app.get('/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  )
+
+  app.get('/auth/google/callback',
+    passport.authenticate('google')
+  )
+}
+
+module.exports = routes
