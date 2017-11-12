73b990a376cb99933b6643cbf12a5bcb25057f35
Mario de Biase <debiasej@gmail.com>
2017-11-12 19:14:37 +0100

    Set a callback url and get user info

--- server/index.js
+++ server/index.js
@@ -12,8 +12,10 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken)
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken)
+      console.log('refresh token', refreshToken)
+      console.log('profile', profile)
     }
   )
 )
@@ -22,6 +24,10 @@ app.get('/auth/google',
   passport.authenticate('google', {
     scope: ['profile', 'email']
   })
+)
+
+app.get('/auth/google/callback',
+  passport.authenticate('google')
 )
 
 const PORT = process.env.PORT || 5000
