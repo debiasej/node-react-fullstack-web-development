e8d589a349b98c727c8f0d6ce7bc672e0ff6ca1e
Mario de Biase <debiasej@gmail.com>
2018-03-18 14:35:40 +0100

    Add a navbar to Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 class Header extends Component {
   render () {
     return (
-      <div>
-                Header
-            </div>
+      <nav>
+        <div className='nav-wrapper'>
+          <a className='left brand-logo'>Email Surveys</a>
+          <ul className='right'>
+            <li><a>Login With Google</a></li>
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
