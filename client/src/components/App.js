2a20196752136b80e2319eb805c5f241afe47afd
Mario de Biase <debiasej@gmail.com>
2018-03-18 21:21:50 +0100

    Fetch user data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
 import Header from './Header'
 
@@ -7,19 +10,28 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SuveryNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-  return (
-    <div className='container'>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/surveys' component={Dashboard} />
-          <Route path='/surveys/new' component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  )
+class App extends Component {
+  componentDidMount () {
+    this.props.fetchUser()
+  }
+  render () {
+    return (
+      <div className='container'>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/surveys' component={Dashboard} />
+            <Route path='/surveys/new' component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App
+App.propTypes = {
+  fetchUser: PropTypes.func.isRequired
+}
+
+export default connect(null, actions)(App)
