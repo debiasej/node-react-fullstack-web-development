0058daaeda5a192d85920c45cdff289b3ccf46c2
Mario de Biase <debiasej@gmail.com>
2018-03-21 21:06:47 +0100

    Show user's login status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderContent () {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return <li><a href='/auth/google'>Login With Google</a></li>
+      default:
+        return <li><a>Logout</a></li>
+    }
+  }
+
   render () {
     return (
       <nav>
         <div className='nav-wrapper'>
           <a className='left brand-logo'>Email Surveys</a>
           <ul className='right'>
-            <li><a>Login With Google</a></li>
+            { this.renderContent() }
           </ul>
         </div>
       </nav>
@@ -15,4 +28,12 @@ class Header extends Component {
   }
 }
 
-export default Header
+Header.propTypes = {
+  auth: PropTypes.object
+}
+
+function mapStateToProps ({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
