f59c94888a54ff0d24363ec07dc169bd704e9383
Mario de Biase <debiasej@gmail.com>
2018-04-08 13:12:09 +0200

    Create a landing page

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Email Surveys!
+      </h1>
+        Collect feedback from users
+    </div>
+  )
+}
+
+export default Landing
