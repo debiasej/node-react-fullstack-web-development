fa5b4a8586fb7e01fdeda3c094105f88ebb7afc3
Mario de Biase <debiasej@gmail.com>
2018-05-06 21:46:55 +0200

    Create a middleware to make sure that user is logged

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,11 +1,9 @@
 const keys = require('../config/keys')
 const stripe = require('stripe')(keys.stripeSecretKey)
+const requireLogin = require('../middlewares/requireLogin')
 
 const routes = app => {
-  app.post('/api/stripe', async (req, res) => {
-    if (!req.user) {
-      res.status(401).send({ error: 'User not authenticated' })
-    }
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     await stripe.charges.create({
       amount: 500,
       currency: 'usd',
