6171f1f672b88c5b959cc3b298fee958282533cd
Mario de Biase <debiasej@gmail.com>
2018-05-06 21:44:49 +0200

    Save new credits into database

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  credits: { type: Number, default: 0 }
 })
 
 // Create a new collection in MongonDB
--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,14 +3,19 @@ const stripe = require('stripe')(keys.stripeSecretKey)
 
 const routes = app => {
   app.post('/api/stripe', async (req, res) => {
-    const charge = await stripe.charges.create({
+    if (!req.user) {
+      res.status(401).send({ error: 'User not authenticated' })
+    }
+    await stripe.charges.create({
       amount: 500,
       currency: 'usd',
       description: '$5 for 5 credits',
       source: req.body.id
     })
 
-    console.log(charge)
+    req.user.credits += 5
+    const user = await req.user.save()
+    res.send(user)
   })
 }
 
