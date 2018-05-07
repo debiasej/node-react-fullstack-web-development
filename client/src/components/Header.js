4d3eabb2533752040ece9ba741af70c2467c355e
Mario de Biase <debiasej@gmail.com>
2018-05-07 20:08:06 +0200

    Show user's credits

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,7 +15,8 @@ class Header extends Component {
       default:
         return [
           <li key='1'><Payments /></li>,
-          <li key='2'><a href='/api/logout'>Logout</a></li>
+          <li key='2' style={{ margin: '0 10px' }}>Credits: {this.props.auth.credits}</li>,
+          <li key='3'><a href='/api/logout'>Logout</a></li>
         ]
     }
   }
