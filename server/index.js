10d14c49f3c249a2b9a39ad581e5b461a530d75a
Mario de Biase <debiasej@gmail.com>
2018-05-15 21:20:36 +0200

    Set routes in production

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cookieSession = require('cookie-session')
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const keys = require('./config/keys')
+const path = require('path')
 
 require('./models/user')
 require('./services/passport')
@@ -34,6 +35,16 @@ app.use(passport.session())
 
 authRoutes(app)
 billingRoutes(app)
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  app.use(express.static('client/build'))
+
+  // Serve up the index.html file if it doesn't recognize the route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
