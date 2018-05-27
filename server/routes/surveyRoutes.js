0b8d0a1e1f198f4e78f738a3b62aaa89b2ab8112
Mario de Biase <debiasej@gmail.com>
2018-05-27 19:16:45 +0200

    Add error handling

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -19,7 +19,16 @@ const routes = (app) => {
     })
 
     const mailer = new Mailer(survey, surveyTemplate(survey))
-    await mailer.send()
+    try {
+      await mailer.send()
+      await survey.save()
+      req.user.credits -= 1
+      const user = await req.user.save()
+
+      res.send(user)
+    } catch (err) {
+      res.status(402).send(err)
+    }
   })
 }
 
