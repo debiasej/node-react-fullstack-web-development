726d617063f43f725a8937420de4860c9487f390
Mario de Biase <debiasej@gmail.com>
2018-06-03 15:23:45 +0200

    Create a dashboard component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,8 +6,8 @@ import * as actions from '../actions'
 
 import Header from './Header'
 import Landing from './Landing'
+import Dashboard from './Dashboard'
 
-const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SuveryNew</h2>
 
 class App extends Component {
