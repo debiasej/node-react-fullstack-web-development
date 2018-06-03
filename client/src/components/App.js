b14429cc417f9afd5849bfb52d371b78db39e5f8
Mario de Biase <debiasej@gmail.com>
2018-06-03 18:37:37 +0200

    Create a component to create new surveys

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,8 +7,7 @@ import * as actions from '../actions'
 import Header from './Header'
 import Landing from './Landing'
 import Dashboard from './Dashboard'
-
-const SurveyNew = () => <h2>SuveryNew</h2>
+import SurveyNew from './surveys/SurveyNew'
 
 class App extends Component {
   componentDidMount () {
